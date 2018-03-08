2644f7aee0b818bf4223078346d51cbb44539051
章青周 <zhangqz@guahao.com>
2018-03-08 17:58:18 +0800

    update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,7 +21,7 @@ module.exports = {
     'arrow-parens': 0,
     // allow async-await
     'generator-star-spacing': 0,
-    'prefer-promise-reject-errors': 1,
+    'prefer-promise-reject-errors': 'off',
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
   }
